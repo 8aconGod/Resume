fa9494ec5fb7d92f6974f69f5fc34c32b1d10c83
8aconGod <blindninjamaster@gmail.com>
2021-02-03 18:25:32 -0800

    Commit 1

--- test.py
+++ test.py
@@ -1,1 +1,45 @@
-print("hello world")
+
+## Ryan Hall
+genericemail@gmail.com | 555-555-5555 | 1234 Johnny Appleseed Lane, Eugene OR, 97478
+___
+
+
+### __Experience__
+
+#### __Amazon__ - _Security Engineer Summer Internship - 2023_
+- Risk assessment for data confidentiality, systems and services
+- conducting security assessments and penetration testing to evaluate security vulnerabilities
+- Develop, test, and review code that supports security protocols
+#### __American Express__ - _Information Security Summer internship - 2022_
+- Deliver cryptographic and key management solutions
+- Monitor and detect malicious attacks to prevent data loss
+- Deliver secure network solutions that enable secure operations
+#### __Oregon Community Credit Union__ - _Service Representative - Dec. 2018 - Summer 2022_
+- Adhering to banking policies and ensuring accuracy in all transactions and data entry
+- Maintaining a high level of professionalism and utilizing problem resolution skills to assist members
+- Maintaining business relationships and providing product recommendations that best serve members
+### __Projects__
+- __Packet Sniffer__
+    *  Created a packet sniffer from scratch for TCP and UDP packets to learn network traffic analysis
+    * comparison to professional assessment tools such as Wireshark
+
+- __Website__
+    * created a website to develop frontend and backend development skills
+    * Learning project management, password encryption and server management
+
+- __Project Links__ - [project](https://github.com/8aconGod/Resume)
+
+### __Education__
+#### Oregon State University
+_Bachelor's Degree in Computer Science_ - _June 2024_
+### __Skills__
+- Javascript, HTML, Python, C++
+- Vulnerability assessment tools (Nikto, Wireshark)
+- Knowledge of programming and scripting for development of security tools and industry frameworks
+
+
+### __Certifications__
+- CompTIA Security+
+- EC-Council CEH v11 Issue Date: 06/05/2021 Certificate Number: 123456789
+- AWS Certified Solutions Architect - Associate -
+    License AWS-ASA 12345
